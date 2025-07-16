f8a323eba1f2500cccb247f38dd010e0b5c1cb58
basmethope <absmt678@gmail.com>
2025-07-16 23:12:12 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState<number>(0);
+
+  const handleClick = () => setCount(count + 1);
+  const reset = () => setCount(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>عداد النقر</h1>
+      <h2>{count}</h2>
+      <button onClick={handleClick} style={{ marginRight: "10px" }}>
+        اضغطني!
+      </button>
+      <button onClick={reset}>إعادة التصفير</button>
     </div>
   );
 }
